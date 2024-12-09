4db68cc89e96bf8f62e978f8d4e5cdbb37ce0e14
Gayasuddin <gyasuansari145@gmail.com>
2024-12-09 11:32:44 +0530

    landing page

--- fullstack/src/App.jsx
+++ fullstack/src/App.jsx
@@ -1,7 +1,9 @@
+import LandingPage from  "./components/LandingPage.jsx"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <LandingPage />
+    </>
   )
 }
